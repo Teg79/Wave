783edba7da42a918e57dce97e9ac2a90dec7b8ea
Francesco Bellentani <francesco.bellentani@gmail.com>
2015-01-01 21:13:17 +0100

    Fixed test with a magnet link, no need to modify the test, just add in your ide the dependency on the right libtorrent library (ie. libtorrent.so in /src/main/resources/binaries) and run it

--- src/test/java/TestTorrent.java
+++ src/test/java/TestTorrent.java
@@ -1,11 +1,9 @@
-import com.frostwire.jlibtorrent.LibTorrent;
-import com.frostwire.jlibtorrent.Session;
-import com.frostwire.jlibtorrent.TorrentAlertAdapter;
-import com.frostwire.jlibtorrent.TorrentHandle;
-import com.frostwire.jlibtorrent.alerts.BlockDownloadingAlert;
+import com.frostwire.jlibtorrent.*;
 import com.frostwire.jlibtorrent.alerts.BlockFinishedAlert;
+import com.frostwire.jlibtorrent.alerts.TorrentAddedAlert;
 import com.frostwire.jlibtorrent.alerts.TorrentFinishedAlert;
 import org.junit.Test;
+import org.slf4j.*;
 
 import java.io.File;
 import java.util.concurrent.CountDownLatch;
@@ -15,41 +13,72 @@ import java.util.concurrent.CountDownLatch;
  */
 public class TestTorrent {
 
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger("test");
+
     @Test
     public void test() throws Throwable {
+        LOGGER.info("Using libtorrent version: " + LibTorrent.version());
 
-        // comment this line for a real application
-        String[] args = new String[]{"/Users/teg/Downloads/The_Hunger_Games_2012_1080p_1080p.torrent"};
+        String uri = "magnet:?xt=urn:btih:86d0502ead28e495c9e67665340f72aa72fe304e";
+        File torrentFile = new File("./src/test/resources/frostwire_installer.torrent");
+        if (torrentFile.exists()) {
+            torrentFile.delete();
+        }
 
-        File torrentFile = new File(args[0]);
+        Session s = new Session();
+        Downloader d = new Downloader(s);
+        DHT dht = new DHT(s);
 
-        System.out.println("Using libtorrent version: " + LibTorrent.version());
+        LOGGER.info("Waiting for nodes in DHT");
+        dht.waitNodes(1);
+        LOGGER.info("Nodes in DHT: " + dht.nodes());
 
-        final Session s = new Session();
+        LOGGER.info("Fetching the magnet uri, please wait...");
+        byte[] data = d.fetchMagnet(uri, 30000);
 
-        final TorrentHandle th = s.addTorrent(torrentFile, torrentFile.getParentFile());
+        if (data != null) {
+            LOGGER.info(Entry.bdecode(data).toString());
+
+            Utils.writeByteArrayToFile(torrentFile, data);
+            LOGGER.info("Torrent data saved to: " + torrentFile);
+        } else {
+            LOGGER.info("Failed to retrieve the magnet");
+        }
+
+
+        File parentFile = torrentFile.getParentFile();
+        s.asyncAddTorrent(torrentFile, parentFile);
+        final TorrentHandle th = s.getTorrents().get(0);
 
         final CountDownLatch signal = new CountDownLatch(1);
 
         s.addListener(new TorrentAlertAdapter(th) {
-            @Override
-            public void blockDownloading(BlockDownloadingAlert alert) {
-                int p = (int) (th.getStatus().getProgress() * 100);
-                System.out.println("Ding Progress: " + p);
-            }
 
             @Override
             public void blockFinished(BlockFinishedAlert alert) {
                 int p = (int) (th.getStatus().getProgress() * 100);
-                System.out.println("Progress: " + p);
+                LOGGER.info("Progress: " + p);
             }
 
             @Override
             public void torrentFinished(TorrentFinishedAlert alert) {
-                System.out.print("Torrent finished");
+                LOGGER.info("Torrent finished");
+                signal.countDown();
+            }
+
+            @Override
+            public void torrentAdded(TorrentAddedAlert alert) {
+                LOGGER.info("Torrent added");
                 signal.countDown();
             }
         });
+
+        File targetFile = new File(parentFile, th.getTorrentInfo().getName());
+        if (targetFile.exists()) {
+            targetFile.delete();
+        }
+        LOGGER.info("Nome torrent: " + th.getTorrentInfo().getName());
+        th.resume();
 
         signal.await();
     }
